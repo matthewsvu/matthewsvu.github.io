19b07e49e2de11f9fcc9b0afd613c63f5c55b771
matthewsvu <msv180000@utdallas.edu>
2021-04-10 12:01:31 -0700

    site fix

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-$(function(){
-  var str = '#len'; //increment by 1 up to 1-nelemnts
-  $(document).ready(function(){
-    var i, stop;
-    i = 1;
-    stop = 4; //num elements
-    setInterval(function(){
-      if (i > stop){
-        return;
-      }
-      $('#len'+(i++)).toggleClass('bounce');
-    }, 500)
-  });
-});
+$(document).ready(function () {
 
+  $('.navbar .dropdown-item').on('click', function (e) {
+      var $el = $(this).children('.dropdown-toggle');
+      var $parent = $el.offsetParent(".dropdown-menu");
+      $(this).parent("li").toggleClass('open');
+
+      if (!$parent.parent().hasClass('navbar-nav')) {
+          if ($parent.hasClass('show')) {
+              $parent.removeClass('show');
+              $el.next().removeClass('show');
+              $el.next().css({"top": -999, "left": -999});
+          } else {
+              $parent.parent().find('.show').removeClass('show');
+              $parent.addClass('show');
+              $el.next().addClass('show');
+              $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+          }
+          e.preventDefault();
+          e.stopPropagation();
+      }
+  });
+
+  $('.navbar .dropdown').on('hidden.bs.dropdown', function () {
+      $(this).find('li.dropdown').removeClass('show open');
+      $(this).find('ul.dropdown-menu').removeClass('show open');
+  });
+
+});
